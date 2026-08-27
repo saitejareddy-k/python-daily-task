8450714b28354ab317fba98f67bcc2dd47f604ff
saitejareddy-k <saitejareddy-k>
2026-08-27 21:14:56 +0530

    number

---
+++ prime.py
@@ -0,0 +1,18 @@
+#function for checking prime numbers 
+def Prime(n):
+    # Checking that n is more than 1 
+    if n > 1;
+       # Iterating over the n 
+       for i in range(2,int(n/2)+1):
+              # If n is dividble or not 
+              if (n % i) == 0;
+                  print(n, "is not a prime number")
+        else:
+            print(n, " is a prime number")
+    # If n is 1 
+    else 
+        print(n, "is not a prime number")
+# Takinng an input from the user
+num = int(input("Enter an input number: "))
+# Printing result 
+Prime(num)
