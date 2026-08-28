440381e32ca8fefceca46b51002838ea9e6494f4
saitejareddy-k <saitejareddykunreddy07@gmail.com>
2026-08-28 22:43:39 +0530

    greater

---
+++ display.py
@@ -0,0 +1,11 @@
+a =int(input("Enter Number 1:"))
+b =int(input("Enter Number 2:"))
+c =int(input("Enter Number 3:"))
+if(a>b and a>c);
+    print(a," is greater")
+elif(b>a and b>c);
+    print(b," is greater")
+elif(c>a and c>b);
+    print(c," is greater")
+else:
+    print("All are same")
