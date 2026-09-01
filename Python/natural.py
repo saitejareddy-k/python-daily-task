9ed4de7931df4f309b2e994acfeaa3165b51fd30
saitejareddy-k <saitejareddykunreddy07@gmail.com>
2026-09-01 21:37:23 +0530

    next one

---
+++ Python/natural.py
@@ -0,0 +1,12 @@
+limit
+= int (input ("Enter the limit: "
+# Initialize the sum
+sum = 0
+# Use a for loop to calculate the sum of natural numbers
+for i in range(1, limit
+sum += i
+# Print the sum
++ 1):
+print ( "The sum of natural numbers up to"
+Enter the limit: 10
+The sum of natural numbers up to 10 is:"is:" , sum
